7c98c256a6f26c25df7dba1ee7b3acaefa2af5c0
Jason McConnell <jasonmcc@outlook.com>
2015-11-07 10:24:07 -0800

    JSON console out

--- tuts-learning/js/ajax.js
+++ tuts-learning/js/ajax.js
@@ -3,6 +3,9 @@
 	//ajax limited to getting data from the same domain for security purposes
 	
 	var xhr = new XMLHttpRequest();
+	xhr.onload = function () {
+		console.log(JSON.parse(xhr.responseText));
+	};
 	console.log(xhr);
 	
 	xhr.open('GET', 'js/data.json');
